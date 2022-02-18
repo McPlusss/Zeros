ad8a2222766bb9ae99eeaa2ecbabc4dac0e81585
McPlus <97387028+McPlusss@users.noreply.github.com>
2022-02-18 19:41:47 +0800

    新增从WebSocket接收到的JSON的Java对象

---
+++ src/main/java/cn/pac/zeros/WebSocket/WebSocketJsonObject.java
@@ -0,0 +1,12 @@
+package cn.pac.zeros.WebSocket;
+
+/**
+ * 从WebSocket接收到的JSON的Java对象
+ *
+ * @author McPlus
+ * @date 2022/2/18
+ */
+public class WebSocketJsonObject {
+    public WebSocketJsonType type;
+    public String data;
+}
